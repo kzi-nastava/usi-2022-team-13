a02d0eb8aac42d6bf5e55bd65ac45769ca311861
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-04-20 19:21:43 +0200

    Created the Database and The Project
    Created a MS Access database and created the project repository with basic hierarchy. Also implemented basic methods for using scripts to insert into database. This was a team job, we all worked together on it.

---
+++ HealthCareSystemTeam13/HealthCareSystem/HealthCareSystem/Core/Scripts/Repository/InsertionRepository.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace HealthCareSystem.Core.Scripts.Repository
+{
+    class InsertionRepository
+    {
+        private static OleDbConnection Connection;
+
+        public InsertionRepository()
+        {
+            try
+            {
+                Connection = new OleDbConnection();
+
+                Connection.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=HCDb.mdb;
+                Persist Security Info=False;";
+
+                Connection.Open();
+
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+            }
+        }
+
+
+        public void ExecuteQueries()
+        {
+
+            InsertUsers();
+            InsertDoctors();
+            InsertManager();
+            InsertPatients();
+            InsertSecretaries();
+            InsertSurveys();
+
+
+            Connection.Close();
+        }
+
+
+        public void DeleteRecords()
+        {
+            try
+            {
+                Connection.Open();
+
+            }catch(Exception exception)
+            {
+
+            }
+
+            var query = "Delete from users";
+            using (var cmd = new OleDbCommand(query, Connection))
+            {
+                cmd.ExecuteNonQuery();
+
+            }
+
+            Connection.Close();
+
+        }
+        public static void InsertUsers()
+        {
+            var query = "INSERT INTO users(usrnm, pass, role) VALUES(@usrnm, @pass, @role)";
+            using (var cmd = new OleDbCommand(query, Connection))
+            {
+                cmd.Parameters.AddWithValue("@usrnm", "markoni");
+                cmd.Parameters.AddWithValue("@pass", "markoni123");
+                cmd.Parameters.AddWithValue("@role", "Patient");
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+
+        public static void InsertDoctors()
+        {
+
+        }
+        public static void InsertPatients()
+        {
+
+        }
+        public static void InsertSecretaries()
+        {
+
+        }
+        public static void InsertManager()
+        {
+
+        }
+        public static void InsertSurveys()
+        {
+
+        }
+
+    }
+}
---
+++ HealthCareSystemTeam13/HealthCareSystem/HealthCareSystem/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using HealthCareSystem.Core.Scripts.Repository;
+
+namespace HealthCareSystem
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+            string username = tbUsername.Text;
+            string password = tbPassword.Text;
+
+            int user = CheckLoginInput(username, password);
+
+        }
+
+        private static int CheckLoginInput(string username, string password)
+        {
+            /*
+                Checks if the input is valid, if it is check with database, if that's valid
+                then proceed with login.
+                
+            */
+            bool isValid =  IsValidInput(username, password);
+
+            return 1;
+
+        }
+
+        private static bool IsValidInput(string username, string password)
+        {
+            if (username == "" || password == "" || username.Contains(" "))
+            {
+                MessageBox.Show("Please fulfill both fields.", "Failed Login!");
+                return false;
+            }
+
+
+            MessageBox.Show("Sucesfull  Login. Welcome " + username, "Succesfull Login!");
+
+            return true;
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+           InsertionRepository dbInsertion = new InsertionRepository();
+           dbInsertion.ExecuteQueries();
+
+        }
+
+        private void LoginForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            InsertionRepository dbInsertion = new InsertionRepository();
+            dbInsertion.DeleteRecords();
+        }
+    }
+}
